d1b29f6ae80521eedd49fbd9112892075190b31b
fengshijie22 <110600285+fengshijie22@users.noreply.github.com>
2022-08-24 08:55:53 +0800

    Update game.h

--- game_2/game_2/game.h
+++ game_2/game_2/game.h
@@ -15,4 +15,6 @@ void Initboard(char board[ROWS][COLS], int rows, int cols,char set);
 
 void Displayboard(char board[ROWS][COLS], int row, int col);
 
-void Setmine(char mine[ROWS][COLS],char show[ROWS][COLS], int row, int col);
+void Setmine(char mine[ROWS][COLS], int row, int col);
+
+void Findmine(char mine[ROWS][COLS], char show[ROWS][COLS], int row, int col);
